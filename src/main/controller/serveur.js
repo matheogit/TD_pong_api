c2d58e531b99c61a85c561b5f7ce87a400e839fd
Mathis Cadio <cadio@et.esiea.fr>
2021-12-06 15:37:29 +0100

    init gitignore, serveur.js, package.json

---
+++ src/main/controller/serveur.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var serve_static = require('serve-static');
+var http = require('http');
+
+var app = express();
+
+app.use(serve_static(__dirname+"/src/main/vue"));
+var serveur = http.Server(app);
+serveur.listen(8080, function(){});
