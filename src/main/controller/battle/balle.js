d5aa8671ab971ca7ff88b6943b0917e0601231bb
Mathis Cadio <cadio@et.esiea.fr>
2021-12-07 14:19:30 +0100

    controller balle

--- src/main/controller/battle/balle.js
+++ src/main/controller/battle/balle.js
@@ -1,8 +1,11 @@
+var partie = require("./partie")
+
 module.exports = {
   getRaquettePos: function (req, res) {
-    let positionX = 0, positionY = 0;
-    [positionX, positionY] = getBallePos()
+    let game = partie.getGame()
+    let positionX = game.balle.getPosX()
+    let positionY = game.balle.getPosY()
     let result = {"pos_x": positionX, "pos_y": positionY}
-    res.status(200).json(result)
+    res.send(result)
   }
 }
