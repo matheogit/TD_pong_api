95261a23b728c6628b0793e644e99a8637904d21
YoanGuerineau <yoan.guerineau@live.fr>
2021-12-07 13:11:23 +0100

    Tests Socket.io rooms

--- index.js
+++ index.js
@@ -5,23 +5,41 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-let ongoingGames = {};
+const clients = {};
+const games = {};
+
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/game.html');
 });
 
 io.on('connection', (socket) => {
-	socket.on('chat message', (msg) => {
-		io.emit('chat message', msg);
-	});
-	socket.on('move', (msg) => {
-		io.emit('move', msg);
-		if( msg.contains("quit") ) {
-			hash = msg.split(':')[0];
-			console.log(msg);
-			ongoingGames.get(hash).stop();
+	socket.on('create', () => {
+		let newGameId = guid();
+		games[newGameId] = {
+			"gameId": newGameId,
+			"clients": [],
 		}
+		socket.emit('create', {"gameId": newGameId});
+	});
+	socket.on('join', (data) => {
+		let gameId = data.gameId;
+		if ( (! games[gameId]) || games[gameId].clients.length >= 2 ) {
+			return;
+		}/*
+		if ( games[gameId].clients.length == 2 ) {
+			updateGameState();
+		}*/
+		games[gameId].clients[games[gameId].clients.length] = socket;
+		games[gameId].clients.forEach(clientSocket => {
+			clientSocket.emit('test');
+			console.log(clientSocket.id);
+		});
+		console.log(games[gameId].clients.length);
+		socket.on('move', (data) => {
+			io.emit('move', data);
+		});
 	});
 });
 
@@ -29,18 +47,9 @@ server.listen(3000, () => {
 	console.log('listening on *:3000');
 });
 
-app.get('/sologame', (req, res) => {
-	gameHash = getRandomHash();
-	newPath = '/sologame/'+gameHash;
-	res.redirect(newPath);
-	console.log(ongoingGames);
-	app.get(newPath, (req, res) => {
-		res.sendFile(__dirname + '/game.html');
-		ongoingGames[gameHash] = res;
-	})
-});
-
-function getRandomHash() {
-	let val = Math.random().toString(36).substring(2, 15);
-	return val;
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
+ 
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
