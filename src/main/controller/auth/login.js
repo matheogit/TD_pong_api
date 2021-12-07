fe93ccd15d6ec38628d8ad0e32ba412c74958145
blasseye <lasseye@et.esiea.fr>
2021-12-07 14:02:38 +0100

    Rectification, recupération du body

--- src/main/controller/auth/login.js
+++ src/main/controller/auth/login.js
@@ -2,6 +2,7 @@ module.exports = {
     send: function (req, res){
         let pwd = req.params.pwd //recupération du mot de passe en paramètre
         let user = req.params.user//recupération dunom d'utilisateur en paramètre
-        res.send("les données suivantes sont envoyées : \n"+ user+ "\n" +pwd);
+        let result = {"User": user,"Psw": pwd}
+        res.send(result);
     },
 }
--- src/main/controller/auth/register.js
+++ src/main/controller/auth/register.js
@@ -3,6 +3,7 @@ module.exports = {
         let email = req.params.email //recupération du email en paramètre
         let user = req.params.user//recupération dunom d'utilisateur en paramètre
         let pwd = req.params.pwd //recupération du mot de passe en paramètre
-        res.send("les données suivantes sont envoyées : \n"+email + "\n"+ user+ "\n" +pwd);
+        let result = {"Email": email, "User": user,"Psw": pwd}
+        res.send(result);
     },
 }
