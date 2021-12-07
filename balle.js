32bd3b9799ab9ceebde8053dab5d5d7aa9edc175
Clément DUBOIS <clemdub72@gmail.com>
2021-12-07 09:40:44 +0100

    Ajout classe balle et raquette (setter/getter)

---
+++ balle.js
@@ -0,0 +1,14 @@
+class balle{
+    constructor(posX,posY){
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    get getPosX(){
+        return this.posX;
+    }
+
+    get getPosY(){
+        return this.posY;
+    }
+}
