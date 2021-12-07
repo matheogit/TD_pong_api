fc28d1a97d7006a516ebf2f832605e6509c839c1
Mathis Cadio <cadio@et.esiea.fr>
2021-12-07 10:38:57 +0100

    Routes battle

---
+++ src/main/controller/battle/routesBattle.js
@@ -0,0 +1,23 @@
+var balle = require("./balle")
+var raquette = require("./raquette")
+var score = require("./score")
+var partie = require("./partie")
+
+var express = require('express');
+var router = express.Router();
+
+/*********BALLE*********/
+router.route('/balle').get(balle.getRaquettePos);
+
+/*********RAQUETTE*********/
+router.route('/raquette/:idJoueur').get(raquette.getRaquettePos);
+router.route('/raquette/:idJoueur/:pos').post(raquette.postRaquettePos);
+
+/*********SCORE*********/
+router.route('/score/:idJoueur').get(score.getScore);
+
+/*********PARTIE*********/
+router.route('/etat').get(partie.getEtatPartie);
+
+
+module.exports = router;
--- src/main/controller/serveur.js
+++ src/main/controller/serveur.js
@@ -2,8 +2,12 @@ var express = require('express');
 var serve_static = require('serve-static');
 var http = require('http');
 
+var routesBattle = require('./battle/routesBattle');
+
 var app = express();
 
 app.use(serve_static(__dirname+"/src/main/vue"));
 var serveur = http.Server(app);
 serveur.listen(8080, function(){});
+/********************BATTLE*********************/
+app.use('/battle', routesBattle);
