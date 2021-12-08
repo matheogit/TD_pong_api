b480b0339e8a2a35391e4c70750a9eb35360019b
blasseye <lasseye@et.esiea.fr>
2021-12-08 15:23:40 +0100

    ajout de commentaire

--- Controller/auth/register.js
+++ Controller/auth/register.js
@@ -1,22 +1,22 @@
 module.exports = {
     regis: function (req, res){
-        let email = req.body.email //recupération du email en paramètre
-        let user = req.body.username //recupération dunom d'utilisateur en paramètre
-        let pwd = req.body.passeword //recupération du mot de passe en paramètre
-        let result = {"mail": email, "user": user,"pwd": pwd}
-        axios.post('https://auth-pong-esiea.azurewebsites.net/register', {
+        let email = req.body.email      //recupération du email en paramètre
+        let user = req.body.username    //recupération dunom d'utilisateur en paramètre
+        let pwd = req.body.passeword    //recupération du mot de passe en paramètre
+        let result = {"mail": email, "user": user,"pwd": pwd} //création du body a envoyé a l'auth
+        axios.post('https://auth-pong-esiea.azurewebsites.net/register', { //TO DO changer l'adresse par une variable d'environnement
                 body: body
             })
             .then(function (response) {
-                console.log(response.status);
-                console.log(response.body);
-                let result = {"status":response.status,"data":response.body};
-                res.send(result);
+                console.log(response.status);   //affichage du status dans la console
+                console.log(response.body);     //affichage du body dans la console
+                let result = {"status":response.status,"data":response.body}; //création du body a retourné
+                res.send(result);               //envoie du body a l'IHM
             })
             .catch(function (error) {
-                console.log(error);
-                let result = {"status":error.status,"data":{"message":error}};
-                res.send(result);
+                console.log(error);             //affichage de l'erreur dans la console
+                let result = {"status":error.status,"data":{"message":error}}; //création du body a retourné
+                res.send(result);               //envoie du body a l'IHM
             });
     },
 }
