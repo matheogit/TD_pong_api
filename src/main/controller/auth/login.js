59e8787d85a5a77867853354359400bba3574910
blasseye <lasseye@et.esiea.fr>
2021-12-08 13:20:04 +0100

    normalement ca marche (changement de l'URL a faire)

--- src/main/controller/auth/login.js
+++ src/main/controller/auth/login.js
@@ -2,23 +2,19 @@ module.exports = {
     log: function (req, res, next){
         let user = req.body.username //recupération dunom d'utilisateur en paramètre
         let pwd = req.body.passeword //recupération du mot de passe en paramètre
-        let body = {"User": user,"Pwd": pwd}
-        axios.post('https://svc-pong-esiea.azurewebsites.net/login', {
+        let body = {"user": user,"pwd": pwd}
+        axios.post('https://auth-pong-esiea.azurewebsites.net/login', {
                 body: body
             })
             .then(function (response) {
-                console.log(response);
+                console.log(response.status);
+                console.log(response.body);
+                let result = {"status":response.status,"data":response.body};
+                res.send(result);
             })
             .catch(function (error) {
                 console.log(error);
-            });
-        let status = a;
-        let data = [];
-        axios.get('https://svc-pong-esiea.azurewebsites.net/login')
-            .then(function (response) {
-                console.log(response.status);
-                console.log(response.data);
-                let result = {"status":response.status,"data":response.data};
+                let result = {"status":error.status,"data":{"message":error}};
                 res.send(result);
             });
     },
--- src/main/controller/auth/register.js
+++ src/main/controller/auth/register.js
@@ -3,24 +3,20 @@ module.exports = {
         let email = req.body.email //recupération du email en paramètre
         let user = req.body.username //recupération dunom d'utilisateur en paramètre
         let pwd = req.body.passeword //recupération du mot de passe en paramètre
-        let result = {"Email": email, "User": user,"Pwd": pwd}
-        axios.post('https://svc-pong-esiea.azurewebsites.net/register', {
-            body: body
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    let status = a;
-    let data = [];
-    axios.get('https://svc-pong-esiea.azurewebsites.net/register')
-        .then(function (response) {
-            console.log(response.status);
-            console.log(response.data);
-            let result = {"status":response.status,"data":response.data};
-            res.send(result);
-        });
+        let result = {"mail": email, "user": user,"pwd": pwd}
+        axios.post('https://auth-pong-esiea.azurewebsites.net/register', {
+                body: body
+            })
+            .then(function (response) {
+                console.log(response.status);
+                console.log(response.body);
+                let result = {"status":response.status,"data":response.body};
+                res.send(result);
+            })
+            .catch(function (error) {
+                console.log(error);
+                let result = {"status":error.status,"data":{"message":error}};
+                res.send(result);
+            });
     },
 }
