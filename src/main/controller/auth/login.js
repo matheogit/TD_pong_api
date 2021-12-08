19ed5b4d90ea624c079d9f136e51db2165457f07
blasseye <lasseye@et.esiea.fr>
2021-12-08 11:03:54 +0100

    Login et register

--- src/main/controller/auth/login.js
+++ src/main/controller/auth/login.js
@@ -3,10 +3,23 @@ module.exports = {
         let user = req.body.username //recupération dunom d'utilisateur en paramètre
         let pwd = req.body.passeword //recupération du mot de passe en paramètre
         let body = {"User": user,"Pwd": pwd}
-        httpRequest()
-        httpRequest();
+        axios.post('https://svc-pong-esiea.azurewebsites.net/login', {
+                body: body
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         let status = a;
         let data = [];
+        axios.get('https://svc-pong-esiea.azurewebsites.net/login')
+            .then(function (response) {
+                console.log(response.status);
+                console.log(response.data);    
+            });
+        
         let result = {"status":status,"data":data};
         res.send(result);
     },
--- src/main/controller/auth/register.js
+++ src/main/controller/auth/register.js
@@ -4,10 +4,23 @@ module.exports = {
         let user = req.body.username //recupération dunom d'utilisateur en paramètre
         let pwd = req.body.passeword //recupération du mot de passe en paramètre
         let result = {"Email": email, "User": user,"Pwd": pwd}
-        httpRequest();
-        let status = a;
-        let data = [];
-        let result = {"status":status,"data":data};
-        res.send(result);
+        axios.post('https://svc-pong-esiea.azurewebsites.net/register', {
+            body: body
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    let status = a;
+    let data = [];
+    axios.get('https://svc-pong-esiea.azurewebsites.net/register')
+        .then(function (response) {
+            console.log(response.status);
+            console.log(response.data);
+            let result = {"status":response.status,"data":response.data};
+            res.send(result);
+        });
     },
 }
